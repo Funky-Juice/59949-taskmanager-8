d86bbda31acb86596809a452af54661b9b074412
Иншаков Сергей <inshakovsa@edu.mos.ru>
2019-03-01 12:42:02 +0300

    разбил код на модули

---
+++ src/data/data.js
@@ -0,0 +1,31 @@
+
+export const filtersList = [
+  {
+    name: `all`,
+    count: 15
+  },
+  {
+    name: `overdue`,
+    count: 0
+  },
+  {
+    name: `today`,
+    count: 0
+  },
+  {
+    name: `favorites`,
+    count: 7
+  },
+  {
+    name: `repeating`,
+    count: 2
+  },
+  {
+    name: `tags`,
+    count: 6
+  },
+  {
+    name: `archive`,
+    count: 115
+  }
+];
