2975e345604c9ff5a6a7098a126f3ae0f493026f
Funky-Juice <isa86box@gmail.com>
2019-03-20 23:54:02 +0300

    отрисовка графиков с моковыми данными

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import {renderFilters, renderTasks} from './render-elems';
 import {dataTemplate, filtersList} from './data/data';
 import {generateData} from './utils';
+import './menu';
 
 const tasksData = generateData(dataTemplate);
 
