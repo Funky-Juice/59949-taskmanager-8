7395a1021f9a95f8e20dfcdd0a5bf97bfaef2872
Funky-Juice <isa86box@gmail.com>
2019-03-31 12:19:01 +0300

    проксирование в провайдере методов api

--- src/services/provider.js
+++ src/services/provider.js
@@ -1,2 +1,22 @@
 
-export default class Provider {}
+export default class Provider {
+  constructor({api}) {
+    this._api = api;
+  }
+
+  updateTask({id, data}) {
+    return this._api.updateTask({id, data});
+  }
+
+  createTask({task}) {
+    return this._api.createTask({task});
+  }
+
+  deleteTask({id}) {
+    return this._api.deleteTask({id});
+  }
+
+  getTasks() {
+    return this._api.getTasks();
+  }
+}
