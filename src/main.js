27f99fc1904facf317798d8f928132e230d22437
Funky-Juice <isa86box@gmail.com>
2019-03-31 15:50:39 +0300

    наполнил в store методы для работы с localStorage

--- src/main.js
+++ src/main.js
@@ -2,20 +2,23 @@ import {renderFilters, renderTasks} from './screen/tasks-screen';
 import {filtersList} from './data/data';
 import Provider from './services/provider';
 import Api from './services/api';
-import Store from './store';
+import Store from './services/store';
 import './menu';
-
-const AUTHORIZATION = `Basic dXNlckBwYXNzd29yZAo=${Math.random()}`;
-const API_URL = `https://es8-demo-srv.appspot.com/task-manager`;
 
 const messageContainer = document.querySelector(`.board__no-tasks`);
 const tasksContainer = document.querySelector(`.board__tasks`);
 
+
+const AUTHORIZATION = `Basic dXNlckBwYXNzd29yZAo=${Math.random()}`;
+const API_URL = `https://es8-demo-srv.appspot.com/task-manager`;
+const TASKS_STORE_KEY = `tasks-store-key`;
+
 const api = new Api(API_URL, AUTHORIZATION);
-const store = new Store();
+const store = new Store({key: TASKS_STORE_KEY, storage: localStorage});
 export const provider = new Provider({api, store});
 
 export let tasksData = [];
+
 
 export const fetchTasks = () => {
   messageContainer.innerText = `Loading tasks...`;
--- src/services/store.js
+++ src/services/store.js
@@ -1,10 +1,43 @@
 
 export default class Store {
-  setItem({key, item}) {}
+  constructor({key, storage}) {
+    this._storeKey = key;
+    this._storage = storage;
+  }
 
-  getItem({key}) {}
+  setItem({key, item}) {
+    const items = this.getAll();
+    items[key] = item;
 
-  removeItem({key}) {}
+    this._storage.setItem(this._storeKey, JSON.stringify(items));
+  }
 
-  getAll() {}
+  getItem({key}) {
+    const items = this.getAll();
+    return items[key];
+  }
+
+  removeItem({key}) {
+    const items = this.getAll();
+    delete items[key];
+
+    this._storage.setItem(this._storeKey, JSON.stringify(items));
+  }
+
+  getAll() {
+    const emptyItems = {};
+    const items = this._storage.getItem(this._storeKey);
+
+    if (!items) {
+      return emptyItems;
+    }
+
+    try {
+      return JSON.parse(items);
+    } catch (e) {
+      // eslint-disable-next-line
+      console.error(`Error parse items. Error: ${e}. Items: ${items}`);
+      return emptyItems;
+    }
+  }
 }
